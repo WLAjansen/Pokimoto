533654e4696c7f092ad140fa5fff0a09ffff78a1
WLAjansen <40787982+WLAjansen@users.noreply.github.com>
2020-06-19 11:51:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/startpagina01/startpagina2.java
@@ -0,0 +1,14 @@
+package com.example.startpagina01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class startpagina2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startpagina2);
+    }
+}
