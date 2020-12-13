e969905a845efff2dd4e124d7db3cac9b727e120
WLAjansen <40787982+WLAjansen@users.noreply.github.com>
2020-12-13 22:36:16 +0100

    v.0.06 - Advanced navigation + project setup
    Tab navigatie en betere normale navigatie is toegevoegd. Daarnaast is het project nu beter ingedeeld en is er een boilerplate voor het importeren van fonts toegevoegd.

--- Applicatie/App.js
+++ Applicatie/App.js
@@ -1,62 +1,141 @@
 import * as React from 'react';
-import { View, SafeAreaView, Text, Button, StyleSheet } from 'react-native';
+import { Button, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-// Import screens 
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import HomeScreen from './app/screens/HomeScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+// import * as Font from 'expo-font';
+import WelcomeAsset from './app/screens/WelcomeAsset';
+import HomeAsset from './app/screens/HomeAsset';
 
-// Home screen: splash page with timer
+// const fetchFonts = () => {
+//   return Font.loadAsync({
+//   'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf'),
+//   'roboto-italic': require('./assets/fonts/Roboto-Italic.ttf'),
+//   'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf')
+//   });
+//   };
 
-function Welcome() {
+function DetailsScreen({ navigation }) {
   return (
-    < SafeAreaView style={styles.container}>
-      <WelcomeScreen />
-    </SafeAreaView>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <WelcomeAsset />
+    </View>
   );
 }
 
-
-// Welcome screen: login and register
-
-function Home({ navigation }) {
+function HomeScreen({ navigation }) {
   return (
-    <SafeAreaView style={styles.container}>
-      <HomeScreen />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <HomeAsset />
+      <TouchableOpacity onPress={() => navigation.navigate('Details')} style={styles.button}>
+        <Text style={styles.buttonText}>Afrekenen</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function SettingsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <HomeAsset />
       <Button
-        title="Go to Welcome"
-        onPress={() => navigation.navigate('Welcome')}
+        title="Go to Details" color="black"
+        onPress={() => navigation.navigate('Details')}
       />
-    </SafeAreaView>
+    </View>
   );
 }
-
-// Navigator: Current page and go back button
-
-const Stack = createStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} options={{ headerStyle: { backgroundColor:'#000000'},  headerTintColor: '#000000', }} />
-        <Stack.Screen name="Welcome" component={Welcome} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-// StyleSheet: all general styles for every page
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: '#000000',
+    flex: 1,
     fontFamily: 'Roboto',
   },
+  button: {
+    backgroundColor: "black",
+    paddingRight: 100,
+    paddingLeft: 100,
+    paddingBottom: 15,
+    paddingTop: 15,
+    borderRadius: 8,
+    color: '#fff',
+  },
+  buttonText: {
+    fontSize: 14,
+    color: '#fff',
+  }, 
 });
 
+const HomeStack = createStackNavigator();
 
-export default App;
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} options={{ headerStyle: { backgroundColor:'#000000'},  headerTintColor: '#fff', }} />
+      <HomeStack.Screen name="Details" component={DetailsScreen} options={{ headerStyle: { backgroundColor:'#000000'},  headerTintColor: '#fff', }} />
+    </HomeStack.Navigator>
+  );
+}
+
+const SettingsStack = createStackNavigator();
+
+function SettingsStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Settings" component={SettingsScreen} options={{ headerStyle: { backgroundColor:'#000000'},  headerTintColor: '#fff', }} />
+      <SettingsStack.Screen name="Details" component={DetailsScreen} options={{ headerStyle: { backgroundColor:'#000000'},  headerTintColor: '#fff', }} />
+    </SettingsStack.Navigator>
+  );
+}
+
+const DetailsStack = createStackNavigator();
+
+function DetailsStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Details" component={SettingsScreen} options={{ headerStyle: { backgroundColor:'#000000'},  headerTintColor: '#fff', }} />
+      <SettingsStack.Screen name="Settings" component={DetailsScreen} options={{ headerStyle: { backgroundColor:'#000000'},  headerTintColor: '#fff', }} />
+    </SettingsStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            } else if (route.name === 'Details') {
+              iconName = focused ? 'ios-add-circle' : 'ios-add-circle-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'white',
+          inactiveTintColor: 'gray',
+          inactiveBackgroundColor: 'black',
+          activeBackgroundColor: 'black',
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Settings" component={SettingsStackScreen} />
+        <Tab.Screen name="Details" component={DetailsStackScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ Applicatie/app/screens/WelcomeAsset.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function WelcomeScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>Hi whatsup</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+    
+    },
+    title: {
+     
+    },
+  });
