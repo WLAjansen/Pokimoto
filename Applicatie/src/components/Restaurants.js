c1d93994707a190fdafd9d16921617c9af93ef28
WLAjansen <40787982+WLAjansen@users.noreply.github.com>
2020-12-28 16:26:22 +0100

    Update Restaurants.js

--- Applicatie/src/components/Restaurants.js
+++ Applicatie/src/components/Restaurants.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import { Text, Image, View, StyleSheet, ScrollView } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
 class ScrollViewExample extends Component {
    state = {
       restaurants: [
-         {'name': 'Ben', 'id': 1},
-         {'name': 'Susan', 'id': 2},
-         {'name': 'Robert', 'id': 3},
-         {'name': 'Mary', 'id': 4},
-         {'name': 'Daniel', 'id': 5},
-         {'name': 'Laura', 'id': 6},
-         {'name': 'John', 'id': 7},
-         {'name': 'Debra', 'id': 8},
-         {'name': 'Aron', 'id': 9},
-         {'name': 'Ann', 'id': 10},
-         {'name': 'Steve', 'id': 11},
-         {'name': 'Olivia', 'id': 12}
+         {'name': 'Restaurant one', 'image': {uri: 'https://res.cloudinary.com/donglyhya/image/upload/v1516817597/campaign6_lfiwwo.jpg'}, 'id': 1},
+         {'name': 'Restaurant one', 'image': {uri: 'https://res.cloudinary.com/donglyhya/image/upload/v1516817597/campaign6_lfiwwo.jpg'}, 'id': 2},
+         {'name': 'Restaurant one', 'image': {uri: 'https://res.cloudinary.com/donglyhya/image/upload/v1516817597/campaign6_lfiwwo.jpg'}, 'id': 3},
+         {'name': 'Restaurant one', 'image': {uri: 'https://res.cloudinary.com/donglyhya/image/upload/v1516817597/campaign6_lfiwwo.jpg'}, 'id': 4},
+         {'name': 'Restaurant one', 'image': {uri: 'https://res.cloudinary.com/donglyhya/image/upload/v1516817597/campaign6_lfiwwo.jpg'}, 'id': 5},
+         {'name': 'Restaurant one', 'image': {uri: 'https://res.cloudinary.com/donglyhya/image/upload/v1516817597/campaign6_lfiwwo.jpg'}, 'id': 6},
+         {'name': 'Restaurant one', 'image': {uri: 'https://res.cloudinary.com/donglyhya/image/upload/v1516817597/campaign6_lfiwwo.jpg'}, 'id': 7},
+         {'name': 'Restaurant one', 'image': {uri: 'https://res.cloudinary.com/donglyhya/image/upload/v1516817597/campaign6_lfiwwo.jpg'}, 'id': 8},
+         {'name': 'Restaurant one', 'image': {uri: 'https://res.cloudinary.com/donglyhya/image/upload/v1516817597/campaign6_lfiwwo.jpg'}, 'id': 9},
+         {'name': 'Restaurant one', 'image': {uri: 'https://res.cloudinary.com/donglyhya/image/upload/v1516817597/campaign6_lfiwwo.jpg'}, 'id': 10},
+         {'name': 'Restaurant one', 'image': {uri: 'https://res.cloudinary.com/donglyhya/image/upload/v1516817597/campaign6_lfiwwo.jpg'}, 'id': 11},
+         {'name': 'Restaurant one', 'image': {uri: 'https://res.cloudinary.com/donglyhya/image/upload/v1516817597/campaign6_lfiwwo.jpg'}, 'id': 12}
       ]
    }
    render() {
@@ -24,7 +25,9 @@ class ScrollViewExample extends Component {
                {
                   this.state.restaurants.map((item, index) => (
                      <View key = {item.id} style = {styles.item}>
-                        <Text>{item.name}</Text>
+                         <Image style={styles.innerLogo} source={item.image} />
+                        <Text style={styles.innerHeader}>{item.name}</Text>
+                        <AntDesign style={styles.innerIcon} name="right" size={12} color="white" />
                      </View>
                   ))
                }
@@ -37,14 +40,29 @@ export default ScrollViewExample
 const styles = StyleSheet.create ({
    item: {
       flexDirection: 'row',
-      justifyContent: 'space-between',
       alignItems: 'center',
       alignSelf: 'center',
+      justifyContent: 'space-between',
       width: '90%',
-      padding: 30,
-      margin: 2,
-      borderColor: '#2a4944',
+      margin: 10,
+      borderRadius: 8,
+      padding: 10,
+      borderColor: '#343434',
       borderWidth: 1,
-      backgroundColor: '#d2f7f1'
-   }
+      backgroundColor: '#000000',
+      fontFamily: 'Sharp-Sans-Bold'
+   },
+   innerLogo: {
+    flex: 0.25  ,
+    width: 50,
+    height: 50,
+    borderRadius: 8,
+  },
+  innerHeader: {
+    flex: 0.5,
+      color: 'white',
+  },
+  innerIcon: {
+    flex: 0.1,
+  }
 })
